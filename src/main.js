72b02ad74e8bf4b815c7aa3ebfc4ea683cc47e59
Raissa Lima <raissa.rib@unifor.br>
2023-11-15 22:06:00 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+// Importar arquivos CSS do Bootstrap e Bootstrap Vue
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// Instalar o Bootstrap Vue
+Vue.use(BootstrapVue);
+// Opcionalmente instalar o plugin de ícones do Bootstrap Vue
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+
